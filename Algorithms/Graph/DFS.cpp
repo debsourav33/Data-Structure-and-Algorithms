e33652fb6c61c7d04d4577a4a6979d9cada7d222
debsourav33 <debsourav33@gmail.com>
2018-09-13 21:03:56 +0600

    Gitignore

--- Algorithms/Graph/DFS.cpp
+++ Algorithms/Graph/DFS.cpp
@@ -2,53 +2,58 @@
 using namespace std;
 
 //{
-#define ceil(n,k) ((n-1)/k+1)
+#define cel(n,k) ((n-1)/k+1)
 #define sets(a) memset(a, -1, sizeof(a))
 #define clr(a) memset(a, 0, sizeof(a))
 #define max(a,b) ((a)>(b)? (a):(b))
 #define min(a,b) ((a)<(b)? (a):(b))
 #define fr(n) for(int i=0;i<n;i++)
-#define ull unsigned long long
-#define ll long long
 #define pb push_back
-#define INF 10000007
 #define all(v) v.begin(),v.end()
+#define mp make_pair
+#define ff first
+#define ss second
+#define INF 10000007
+
+typedef long l;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<long long,long long> pll;
 //}
 
-
 vector<int> v[100005];
-int visited[100005], parent[100005], cycle=0;
+int visited[100005], parent[100005], n, edges, cycle=0;
 
 void reset(){
-    clr(visited);
+    clr(vis);
     sets(parent);
 }
 
 void dfs(int u){
-    visited[u]= 1;
+    vis[u]= 1;
 
     fr(v[u].size()){
         int node= v[u][i];
-        if(node==parent[u]) continue;
+        if(nd==parent[u]) continue;
 
-        if(!visited[node]){
-            parent[node]= u;
-            dfs(node);
+        if(!vis[nd]){
+            parent[nd]= u;
+            dfs(nd);
         }
 
-        if(visited[node]==1){
+        if(vis[nd]==1){
             cycle= 1;
             return;
         }
     }
 
-
-    visited[u]= 2;
+    vis[u]= 2;
 }
 
 main(){
     reset();
-    int a, b, n, edges;
+    int a, b;
 
     cin>>n>>edges;
 
@@ -60,7 +65,7 @@ main(){
     }
 
     fr(n){
-        if(visited[i]==0)
+        if(vis[i]==0)
             dfs(i);
     }
 
